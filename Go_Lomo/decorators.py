074297c8c10902be776aede59549c0d5a6140116
Alina-SGC <162033434+Alina-SGC@users.noreply.github.com>
2025-01-03 21:50:10 +0500

    Add files via upload

---
+++ Go_Lomo/decorators.py
@@ -0,0 +1,17 @@
+def decorate_message(message):
+    """
+    Декорирует сообщение, добавляя эмодзи и форматирование.
+    """
+    return f"✨ {message} ✨"
+
+def emphasize(text):
+    """
+    Подчеркивает текст для выделения.
+    """
+    return f"*{text}*"
+
+def highlight(text):
+    """
+    Выделяет текст с помощью символов.
+    """
+    return f"***{text}***"
